32dcfe622fc2dd213f593f83fc2a5fe438df0730
AlvaroCoder <papux45@gmail.com>
2025-08-12 06:22:52 -0500

    vista de id del vehiculo

--- src/app/dashboard/contracts/vehiculo/[idContract]/page.js
+++ src/app/dashboard/contracts/vehiculo/[idContract]/page.js
@@ -1,22 +1,19 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import View1ContractConstitucion from '@/components/Views/View1ContractConstitucion';
+import View2ContractCompraVenta from '@/components/Views/View2ContractCompraVenta';
+import View3ContractsConstitucionFirma from '@/components/Views/View3ContractsConstitucionFirma';
+import View4ContractParteNotarial from '@/components/Views/View4ContractParteNotarial';
 import { useContractDetails } from '@/hooks/useContractsDetails';
 import { useFetch } from '@/hooks/useFetch';
-import { statusContracts } from '@/lib/commonJSON';
-import { camelCaseToTitle, cn } from '@/lib/utils';
-import { Loader2, User2 } from 'lucide-react';
-import dynamic from 'next/dynamic';
+import { aceptarEscritura, generateScriptMarcaAguaCompraVenta, submitFirmarDocumento } from '@/lib/apiConnections';
+import { formatDateToYMD } from '@/lib/fechas';
 import { useParams, useRouter } from 'next/navigation';
-import React, { Suspense } from 'react'
-
-// ✅ Dynamic imports
-const Title1 = dynamic(() => import('@/components/elements/Title1'));
-const CompradoresList = dynamic(() => import('@/components/Tables').then(m => m.CompradoresList));
-const VendedoresList = dynamic(() => import('@/components/Tables').then(m => m.VendedoresList));
+import React, { Suspense, useState } from 'react'
+import { toast } from 'react-toastify';
 
 function RenderPageContracts() {
     const URL_CONTRACT_ID = process.env.NEXT_PUBLIC_URL_HOME_CONTRACT + "/contractId/?idContract=";
-    const router = useRouter();
     const {idContract} = useParams();
     const{
         data : dataResponseContract,
@@ -26,6 +23,84 @@ function RenderPageContracts() {
 
     const {loadingDataClient, client} = useContractDetails(dataResponseContract);
     const dataContract = dataResponseContract?.data || null;
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const [viewPdf, setViewPdf] = useState(null);
+
+      const handleSubmitEscritura=async()=>{
+        try {
+          setLoading(true);
+          const prevData = dataResponseContract?.data;
+          const newDataToSend = {
+            contractId : idContract,
+            ...prevData
+          };
+    
+          const response = await generateScriptMarcaAguaCompraVenta('inmueble', newDataToSend);
+          const blob = await response.blob();
+          const url = URL.createObjectURL(blob);
+    
+          setViewPdf(url);
+          toast("Se genero la escritura",{
+            type :'success',
+            position : 'bottom-right'
+          });
+        } catch (err) {
+          console.error("Error al enviar escritura:", err);
+          toast("Surgio un error al generar la escritura", {
+            type: 'error',
+            position: 'bottom-center'
+          });
+          
+        }finally {
+          setLoading(false);
+        }
+      }
+  const handleCheckViewEscritura=async()=>{
+    try {
+      setLoading(true);
+      const response = await aceptarEscritura(idContract);
+      const responseJSON = await response.json();
+      console.log(responseJSON);
+      toast("La escritura fue aceptada",{
+        type : 'success',
+        position : 'bottom-right'
+      });
+      router.push("/dashboard/contracts");
+
+
+    } catch (err) {
+      console.log(err);
+      
+      toast("Surgio un error al aceptar la escritura",{
+        type : 'error',
+        position : 'bottom-center'
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
+     const handleClickSetFirma=async()=>{
+       try {
+         setLoading(true);
+         const dataToday = formatDateToYMD(new Date());
+         await submitFirmarDocumento(idContract, dataToday);
+         
+         toast("Se firmo el documento",{
+           type:'success',
+           position : 'bottom-right'
+         });
+   
+         router.push("/dashboard")
+       } catch (err) {
+         toast("Surgio un error al generar la escritura",{
+           type : 'error',
+           position : 'bottom-center'
+         });
+       } finally {
+         setLoading(false)
+       }
+     }
     if (loadingDataContract || loadingDataClient) {
         return <div className='p-6'>
           <h1>Cargando contrato ...</h1>
@@ -45,56 +120,70 @@ function RenderPageContracts() {
           </div>
         )
     }
-  const handleButtonContract=(idStatus)=>{
-    if (idStatus === 1) {
-      return(
-        <Button
-          className={"bg-amber-100 rounded-sm p-5 hover:bg-amber-50 cursor-pointer"}
-          variant={"outline"}
-          onClick={()=>router.push("/dashboard/processContract/generateScript/?idContract="+idContract)}
-        >
-          Generar Escritura
-        </Button>
-      )
-    } 
-    if (idStatus === 2) {
-      return(
-        <Button
-          className={"rounded-sm p-6"}
-          variant={"ghost"}
-        >
-          Continuar Revision
-        </Button>
-      )
-    }
+
+  if (loadingDataContract) {
+    return(
+      <div
+      className='p-6 space-y-6'
+    >
+      <h1>Cargando contrato ...</h1>
+    </div>
+    )
   }
+  if (!dataResponseContract || dataResponseContract.data.length === 0) {
     return (
-      <div className='p-8 pb-12 space-y-6 h-screen overflow-y-auto'>
-        <section className='flex flex-row justify-between'>
-          <div>
-            <Title1 className='text-3xl'>Detalles del Contrato de Vehiculos</Title1>
-            <p>Información detallada del contrato de Vehiculos</p> 
-          </div>
-          <div>
-            {handleButtonContract(dataContract?.status)}
-          </div>
-        </section>
-        <section className=''>
-          <p><b>ID: </b>{idContract}</p>
-          <p className='my-1'><b>Estado : </b>{statusContracts?.filter((est)=>est.id === dataContract?.status).map((item)=><span key={item.title} className={cn('px-2 py-1 rounded-sm text-sm space-y-4', item.bgColor)}>{item.title}</span>)}</p>
-          <p><b>Tipo de Contrato :</b> <span>{camelCaseToTitle(dataContract?.contractType)}</span></p>
-          <p className='flex flex-row gap-2'><b>Cliente : </b> <User2/>{loadingDataClient?<Loader2 className='animate-spin'/> : <span>{client?.userName}</span>}</p>
-        </section>
-        <section className='w-full grid grid-cols-1 lg:grid-cols-2 gap-4'>
-          <CompradoresList
-            dataCompradores={dataContract?.buyers?.people || []}
-          />
-          <VendedoresList
-            dataVendedores={dataContract?.sellers?.people || []}
-          />
-        </section>
+      <div className='p-6 space-y-6'>
+        <h1 className='text-3xl font-bold text-gray-800'>Contrato no encontrado</h1>
       </div>
     )
+  }
+  switch (dataContract?.status) {
+    case 1:
+      return(
+        <View1ContractConstitucion
+          idContract={idContract}
+          dataContract={dataContract}
+          loadingDataClient={loadingDataClient}
+          client={client}
+        />
+      )
+  
+      case 2:
+        return(
+          <View2ContractCompraVenta
+            idContract={idContract}
+            dataContract={dataContract}
+            loadingDataClient={loadingDataClient}
+            client={client}
+            loading={loading}
+            viewPdfEscrituraMarcaAgua={handleSubmitEscritura}
+            checkViewEscritura={handleCheckViewEscritura}
+          />
+        )
+      case 3:
+        return(
+          <p>Vista 3</p>
+        )
+      case 4:
+        return(
+          <View3ContractsConstitucionFirma
+            idContract={idContract}
+            dataContract={dataContract}
+            loadingDataClient={loadingDataClient}
+            client={client}
+            handleClickSetFirma={handleClickSetFirma}
+          />
+        )
+      case 5:
+        return(
+          <View4ContractParteNotarial
+            idContract={idContract}
+            dataContract={dataContract}
+            loadingDataClient={loadingDataClient}
+            client={client}
+          />
+        )
+  }
 
 }
 
